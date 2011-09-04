fd624e5c22cda289f03d5d9730bcd637a6b098b4
William Howard <whoward.tke@gmail.com>
2011-09-04 02:22:53 -0400

    use let(x) statement in rspec

--- spec/parser/inject_statement_spec.rb
+++ spec/parser/inject_statement_spec.rb
@@ -1,27 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Cadenza::Parser, 'inject statements' do
-  before(:all) do
-    @parser = Cadenza::Parser.new
-  end
+  let(:parser) { Cadenza::Parser.new }
   
   it "should parse an inject statement" do
-    @parser.parse('{{somevar}}').should have_an_identical_syntax_tree_to "inject.parse.yml"
+    parser.parse('{{somevar}}').should have_an_identical_syntax_tree_to "inject.parse.yml"
   end
   
   it "should parse an inject statement with one filter" do
-    @parser.parse('{{ name | trim }}').should have_an_identical_syntax_tree_to "inject-one-filter.parse.yml"
+    parser.parse('{{ name | trim }}').should have_an_identical_syntax_tree_to "inject-one-filter.parse.yml"
   end
 
   it "should parse an inject statement with multiple filters" do
-    @parser.parse("{{ name | trim | upcase }}").should have_an_identical_syntax_tree_to "inject-multiple-filters.parse.yml"
+    parser.parse("{{ name | trim | upcase }}").should have_an_identical_syntax_tree_to "inject-multiple-filters.parse.yml"
   end
 
   it "should parse an inject statement with filters that have a parameter" do
-    @parser.parse("{{ name | cut: 5 }}").should have_an_identical_syntax_tree_to "inject-one-filter-with-params.parse.yml"
+    parser.parse("{{ name | cut: 5 }}").should have_an_identical_syntax_tree_to "inject-one-filter-with-params.parse.yml"
   end
 
   it "should parse an inject statement with filters which have multiple parameters" do
-    @parser.parse("{{ name | somefilter: 'foo', 3.14159 }}").should have_an_identical_syntax_tree_to "inject-one-filter-with-multiple-params.parse.yml"
+    parser.parse("{{ name | somefilter: 'foo', 3.14159 }}").should have_an_identical_syntax_tree_to "inject-one-filter-with-multiple-params.parse.yml"
   end
 end
