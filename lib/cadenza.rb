2c92137b52a71f0320f1b43966dfd5a6f0811746
William Howard <whoward.tke@gmail.com>
2011-09-04 02:35:32 -0400

    remove references to files which may just be rewritten

--- lib/cadenza.rb
+++ lib/cadenza.rb
@@ -1,11 +1,8 @@
 CADENZA_ROOT = File.expand_path("cadenza", File.dirname(__FILE__)) 
 
-#require File.join CADENZA_ROOT, "filters", "standard"
-#require File.join CADENZA_ROOT, "statements", "standard"
 require File.join CADENZA_ROOT, "token"
 require File.join CADENZA_ROOT, "lexer"
 require File.join CADENZA_ROOT, "parser"
-#require File.join CADENZA_ROOT, "loader"
 require File.join CADENZA_ROOT, "version"
 
 # require all nodes
