9265a32891aae3fd07c80b3f87287cc113b5069f
William Howard <whoward.tke@gmail.com>
2012-02-04 17:22:30 -0500

    cleanup

--- lib/cadenza/nodes/block_node.rb
+++ lib/cadenza/nodes/block_node.rb
@@ -1,13 +1,7 @@
 module Cadenza
-#   class BlockNodeProxy < Struct.new(:context, :stream, :overrides)
-#     def super
-#       return overrides.render(context,stream,true) unless overrides.nil?
-#     end
-#   end
   
    class BlockNode
       attr_accessor :name, :children
-#     attr_accessor :overrides, :overridden_by
 
       def initialize(name, children)
          @name = name
@@ -17,17 +11,6 @@ module Cadenza
       def implied_globals
          @children.map(&:implied_globals).flatten.uniq
       end
-    
-#     def render(context={}, stream='', is_super_call=false)
-#       unless is_super_call or self.overridden_by.nil?
-#         return self.overridden_by.render(context,stream)
-#       end
-      
-#       inner_context = context.clone
-#       inner_context.store('block', BlockNodeProxy.new(context, stream, overrides))
-      
-#       self.children.each { |child| child.render(inner_context,stream) }
-#     end
 
       def ==(rhs)
          @name == rhs.name and
--- lib/cadenza/nodes/document_node.rb
+++ lib/cadenza/nodes/document_node.rb
@@ -18,23 +18,6 @@ module Cadenza
     def implied_globals
       @children.map(&:implied_globals).flatten.uniq
     end
-    
-#     def render(context={}, stream='', overriding_blocks=Hash.new)
-#
-#       overriding_blocks.each do | name, block |
-#         local = self.blocks.fetch(name) rescue next
-#         local.overridden_by = block
-#         block.overrides = local
-#       end
-#
-#       if self.extends.nil?
-#         # There is no base document, render the document as normal
-#         self.children.each { |child| child.render(context,stream) }
-#         return stream
-#       else
-#         extends_template = Loader.get_template('Filesystem', self.extends.eval(context))
-#         return extends_template.render(context, stream, self.blocks)
-#       end
-#     end
+
   end
 end
